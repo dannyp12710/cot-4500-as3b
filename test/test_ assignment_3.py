01a483020b8fb64bd3051db009c535410d7cf271
dannyp12710 <danielpinilla280@gmail.com>
2025-04-04 01:45:16 -0400

    Update test_ assignment_3.py

--- test/test_ assignment_3.py
+++ test/test_ assignment_3.py
@@ -1,1 +1,41 @@
+# For Question 1 
 
+#  Use Gaussian elimination and backward substitution solve the following linear system of equations written in augmented matrix format.
+
+# [[ 2 -1 1 | 6 ]
+# [ 1 3 1  | 0 ]
+# [ -1 5 4 | -3 ]]
+
+
+# For Question 2 
+
+# Implement LU Factorization for the following matrix and do the following:
+
+# a. Print out the matrix determinant.
+# b. Print out the L matrix.
+# c. Print out the U matrix.
+
+# [[ 1 1 0 3 ]
+# [ 2 1 -1 1 ]
+# [ 3 -1 -1 2 ]
+# [ -1 2 3 -1 ]]
+
+
+# For Question 3 
+
+# Determine if the following matrix is diagonally dominate.
+
+# [[ 9 0 5 2 1 ]
+# [ 3 9 1 2 1 ]
+# [ 0 1 7 2 3 ]
+# [ 4 2 3 12 2 ]
+# [ 3 2 4 0 8 ] ]
+
+
+# For Question 4 
+
+# Determine if the matrix is a positive definite.
+
+# [[ 2 2 1 ] 
+# [ 2 3 0 ] 
+# [ 1 0 2 ]]
